801906055b8a5a51e0a5b6fc6e36a0b321562de2
JavaFXpert <james@j1w.xyz>
2019-06-05 06:52:37 -0400

    Incremental progress

--- circuit_grid_model.lua
+++ circuit_grid_model.lua
@@ -1,39 +1,41 @@
 -- CircuitGridModel definitions
-CircuitGridModel = {maxWires = 0, maxColumns = 0}
--- CircuitGridModel = {}
+-- CircuitGridModel = {max_wires = 0, max_columns = 0}
+CircuitGridModel = {}
 
-function CircuitGridModel:new (o, maxWires, maxColumns)
+function CircuitGridModel:new (o, max_wires, max_columns)
     o = o or {}
     setmetatable(o, self)
     self.__index = self
-    self.maxWires = maxWires or 0
-    self.maxColumns = maxColumns or 0
+    self.max_wires = max_wires or 0
+    self.max_columns = max_columns or 0
     return o
 end
 
 function CircuitGridModel:printSelf ()
-    print("maxWires: ", self.maxWires, "maxColumns: ", self.maxColumns)
+    print("max_wires: ", self.max_wires, "max_columns: ", self.max_columns)
 end
 
 ----------------------------------------
 -- CircuitGridNode definitions
 CircuitGridNode = {}
 
-function CircuitGridNode:new (o, maxWires, maxColumns)
+function CircuitGridNode:new (o, node_type, radians, ctrl_a, ctrl_b, swap)
     o = o or {}
     setmetatable(o, self)
     self.__index = self
-    self.maxWires = maxWires or 0
-    self.maxColumns = maxColumns or 0
+    self.node_type = node_type
+    self.radians = radians or 0.0
+    self.ctrl_a = ctrl_a or -1
+    self.ctrl_b = ctrl_b or -1
+    self.swap = swap or -1
+    self.wire_num = -1
+    self.column_num = -1
     return o
 end
 
---[[
---]]
-
 ----------------------------------------
 -- Main
-myCircuit = CircuitGridModel:new(nil,3, 5)
+myCircuit = CircuitGridModel:new(nil,3, 7)
 
 myCircuit:printSelf()
 
