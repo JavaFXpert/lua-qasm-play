f427e03bd7309ad2523f6cf99bd823830d2796a8
JavaFXpert <james@j1w.xyz>
2019-06-05 17:32:52 -0400

    Add initial circuit

--- circuit_grid_model.lua
+++ circuit_grid_model.lua
@@ -18,7 +18,8 @@ function CircuitGridModel:to_string ()
     for wire_num = 1, self.max_wires do
         retval = retval .. '\n'
         for column_num = 1, self.max_columns do
-            retval = retval .. tostring(self.nodes[wire_num][column_num].node_type) .. ', ' 
+            --retval = retval .. tostring(self.nodes[wire_num][column_num].node_type) .. ', ' 
+            retval = retval .. tostring(self.get_node_gate_part(self, wire_num, column_num)) .. ', ' 
             -- TODO: use get_node_gate_part() instead
         end
     end
@@ -52,6 +53,51 @@ function CircuitGridModel:get_node (wire_num, column_num)
     return self.nodes[wire_num][column_num]
 end
 
+function CircuitGridModel:get_node_gate_part (wire_num, column_num)
+    local requested_node = self.nodes[wire_num][column_num]
+    if requested_node and requested_node.node_type ~= CircuitNodeTypes.EMPTY then
+        -- Node is occupied so return its gate
+        return requested_node.node_type
+    else
+        --print("TODO: Handle if node is EMPTY")
+        -- Check for control nodes from gates in other nodes in this column
+        --[[ TODO: make next lines work
+        local nodes_in_column = self.nodes[:, column_num]
+            for idx in range(self.max_wires):
+                if idx != wire_num:
+                    other_node = nodes_in_column[idx]
+                    if other_node:
+                        if other_node.ctrl_a == wire_num or other_node.ctrl_b == wire_num:
+                            return node_types.CTRL
+                        elif other_node.swap == wire_num:
+                            return node_types.SWAP
+        --]]
+    end
+    return CircuitNodeTypes.EMPTY   
+end
+
+--[[
+x    def get_node_gate_part(self, wire_num, column_num):
+x        requested_node = self.nodes[wire_num][column_num]
+x        if requested_node and requested_node.node_type != node_types.EMPTY:
+x            # Node is occupied so return its gate
+x            return requested_node.node_type
+x        else:
+x            # Check for control nodes from gates in other nodes in this column
+x            nodes_in_column = self.nodes[:, column_num]
+            for idx in range(self.max_wires):
+                if idx != wire_num:
+                    other_node = nodes_in_column[idx]
+                    if other_node:
+                        if other_node.ctrl_a == wire_num or other_node.ctrl_b == wire_num:
+                            return node_types.CTRL
+                        elif other_node.swap == wire_num:
+                            return node_types.SWAP
+
+        return node_types.EMPTY
+
+--]]
+
 ----------------------------------------
 -- CircuitGridNode definitions
 CircuitGridNode = {}
@@ -80,10 +126,46 @@ homeDir = os.getenv("HOME")
 dofile (homeDir.."/PycharmProjects/lua-qasm-play/circuit_node_types.lua")
 
 math.randomseed(os.time())
-circuit_grid_model = CircuitGridModel:new{max_wires = 3, max_columns = 7}
+circuit_grid_model = CircuitGridModel:new{max_wires = 3, max_columns = 13}
 
-print(circuit_grid_model:to_string())
+circuit_grid_model:set_node(1, 1, CircuitGridNode:new{node_type = CircuitNodeTypes.X, math.pi/8})
+circuit_grid_model:set_node(2, 1, CircuitGridNode:new{node_type = CircuitNodeTypes.Y, math.pi/6})
+circuit_grid_model:set_node(3, 1, CircuitGridNode:new{node_type = CircuitNodeTypes.Z, math.pi/4})
 
-circuit_grid_model:set_node(1, 2, CircuitGridNode:new{node_type = CircuitNodeTypes.Z, radians = math.pi})
+circuit_grid_model:set_node(1, 2, CircuitGridNode:new{node_type = CircuitNodeTypes.X})
+circuit_grid_model:set_node(2, 2, CircuitGridNode:new{node_type = CircuitNodeTypes.Y})
+circuit_grid_model:set_node(3, 2, CircuitGridNode:new{node_type = CircuitNodeTypes.Z})
+
+circuit_grid_model:set_node(1, 3, CircuitGridNode:new{node_type = CircuitNodeTypes.S})
+circuit_grid_model:set_node(2, 3, CircuitGridNode:new{node_type = CircuitNodeTypes.T})
+circuit_grid_model:set_node(3, 3, CircuitGridNode:new{node_type = CircuitNodeTypes.H})
+
+circuit_grid_model:set_node(1, 4, CircuitGridNode:new{node_type = CircuitNodeTypes.SDG})
+circuit_grid_model:set_node(2, 4, CircuitGridNode:new{node_type = CircuitNodeTypes.TDG})
+circuit_grid_model:set_node(3, 4, CircuitGridNode:new{node_type = CircuitNodeTypes.IDEN})
+
+circuit_grid_model:set_node(3, 5, CircuitGridNode:new{node_type = CircuitNodeTypes.X, 0, 0})
+circuit_grid_model:set_node(2, 5, CircuitGridNode:new{node_type = CircuitNodeTypes.TRACE})
+
+circuit_grid_model:set_node(1, 6, CircuitGridNode:new{node_type = CircuitNodeTypes.IDEN})
+circuit_grid_model:set_node(3, 6, CircuitGridNode:new{node_type = CircuitNodeTypes.Z, math.pi/4, 1})
+
+circuit_grid_model:set_node(3, 7, CircuitGridNode:new{node_type = CircuitNodeTypes.X, 0, 0, 1})
+
+circuit_grid_model:set_node(2, 8, CircuitGridNode:new{node_type = CircuitNodeTypes.H, 0, 2})
+circuit_grid_model:set_node(1, 8, CircuitGridNode:new{node_type = CircuitNodeTypes.IDEN})
+
+circuit_grid_model:set_node(2, 9, CircuitGridNode:new{node_type = CircuitNodeTypes.Y, 0, 0})
+circuit_grid_model:set_node(3, 9, CircuitGridNode:new{node_type = CircuitNodeTypes.IDEN})
+
+circuit_grid_model:set_node(3, 10, CircuitGridNode:new{node_type = CircuitNodeTypes.Z, 0, 0})
+circuit_grid_model:set_node(2, 10, CircuitGridNode:new{node_type = CircuitNodeTypes.TRACE})
+
+circuit_grid_model:set_node(1, 11, CircuitGridNode:new{node_type = CircuitNodeTypes.IDEN})
+
+circuit_grid_model:set_node(3, 12, CircuitGridNode:new{node_type = CircuitNodeTypes.SWAP, 0, 1, -1, 0})
+
+circuit_grid_model:set_node(1, 13, CircuitGridNode:new{node_type = CircuitNodeTypes.X, 0, 1, 2})
+
 print(circuit_grid_model:to_string())
 
